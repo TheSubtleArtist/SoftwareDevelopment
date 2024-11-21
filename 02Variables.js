365e96c6578f0a7c7a349f348c9d9fbee0581324
thesubtleartist <thesubtleartist@yahoo.com>
2024-11-20 21:06:43 -0600

    Chapter 3 Complete

--- 02Variables.js
+++ 02Variables.js
@@ -38,3 +38,86 @@ alert(`My favorite fruit is the ${favFruit}.`);
 
 // math inside of backticks
 alert(`1 + 2 = ${1+2}.`);
+
+
+// data structures
+let arrayObject = [1, "a", 2, "b", 3, "object"];
+let dictObject = {key1:"attribute1", key2:"attribute2"}
+
+// calling the type of a variable when the data type could be unknown in a dynamic variable language
+let dynVariable = "a value";
+console.log(typeof dynVariable);
+
+// assigne a variable as undefined means undefined is both value and type
+let undefVariable = undefined;
+console.log(undefVariable);
+console.log(typeof undefVariable);
+
+//Type Conversions - String - Using predefined type conversions
+
+let numVar = 1;
+console.log(numVar);
+console.log(typeof numVar);
+// convert to string
+numVar = String(numVar);
+console.log(numVar);
+console.log(typeof numVar);
+
+let booVar = true;
+console.log(booVar);
+console.log(typeof booVar);
+booVar = String(booVar); // becomes "true" not true / 1
+console.log(booVar);
+console.log(typeof booVar);
+
+
+// Type Conversion - Number Conversions
+
+let strVar = "5"
+console.log(strVar);
+console.log(typeof strVar);
+//convert to number
+strVar = Number(strVar);
+console.log(strVar);
+console.log(typeof strVar);
+
+// Boolean conversion
+
+let newBooVar = 1; // any number other than 0 becomes true; any string other than 0 becomes true
+console.log(newBooVar)
+console.log(typeof newBooVar);
+
+newBooVar = Boolean(newBooVar);
+console.log(newBooVar)
+console.log(typeof newBooVar);
+
+
+let newerBooVar = 0;
+console.log(newerBooVar)
+console.log(typeof newerBooVar);
+
+newerBooVar = Boolean(newerBooVar);
+console.log(newerBooVar)
+console.log(typeof newerBooVar);
+
+
+// Alert, Prompt, and Confirm Boxes
+
+alert("This Alert Box");
+alert(newerBooVar);
+alert(`My favorite fruit is the ${favFruit}.`);
+
+
+// Prompt Box
+
+//let variable = prompt("String to display to user", defalut_value )
+let age = prompt("Enter the age", 21);
+console.log(age);
+console.log(typeof age);
+alert(`Your age is ${age}`);
+
+// Confirm Box
+
+let confirmVar = ("Do you really need to buy this expensive chocolate?")
+console.log(confirmVar)
+console.log(typeof confirmVar);
